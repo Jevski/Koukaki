dcba3e4e046f5a4605bbb0ca71e913958ed1f239
Jevski <hackettjake00@gmail.com>
2024-01-24 10:53:45 +0100

    full project

---
+++ app/public/wp-content/themes/foce-child/template-parts/oscarSection.php
@@ -0,0 +1,10 @@
+<section id="festival">   
+        <div class="festivalNomination">
+            <p class= "festivalNominationText">  Fleurs d'oranger & chats errants <br>
+             est nomine aux Oscars Short<br> 
+             Filme Animated de 2022! </p> 
+
+             <img class= "oscar-img" src="<?php echo get_stylesheet_directory_uri()  .'/images_koukaki/oscarNomImage.png'; ?>" alt="logo">
+             
+        </div>            
+</section>
